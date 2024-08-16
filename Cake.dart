79d3774cd5e2d29457234e2cb37e58ee74076f8c
Chakhrit1131 <Blue104555@gmail.com>
2024-08-16 10:24:32 +0700

    Add files via upload

---
+++ Cake.dart
@@ -0,0 +1,30 @@
+class Cake {
+  List<String> model=[];
+
+  String? nameShop;
+  String toString()=> 'Name shop is ${nameShop} model is ${model}';
+}
+
+void main(List<String> args) {
+  Cake myCake = Cake();
+  var yourCake = Cake();
+
+  print(myCake.nameShop);
+  print(yourCake.model);
+
+  myCake.nameShop = 'Cakie Shop';
+  myCake.model.addAll(['Cake1','Cake2','Cake3']);
+  print(myCake.nameShop);
+  print(myCake.model);
+
+  print(myCake);
+
+  yourCake.nameShop = 'Birdy';
+  yourCake.model = (['Robusta','late','Cake']);
+
+  print(yourCake.nameShop);
+  print(yourCake.model);
+
+  print(yourCake);
+
+}
---
+++ WorkLaptop.dart
@@ -0,0 +1,18 @@
+import 'Work.dart';
+void main(List<String>args){
+var laptop1 =Laptop(001,'lenovo', 8);
+var laptop2 =Laptop(002, 'lenovo+',12);
+var laptop3 =Laptop(003, 'lenovo++',16);
+laptop1.show();
+laptop2.show();
+laptop3.show();
+
+var house = House();
+house.show();
+
+var car =Car('MSi', 'Blue',4000);
+car.show();
+
+
+
+}
